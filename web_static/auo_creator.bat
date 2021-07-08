9244bf403a73335b9c030c40ea0cd97e051a0814
HernanMontenegro <hernanmontenegroholberton@gmail.com>
2021-07-08 18:54:47 +0000

    4 done!

---
+++ web_static/auo_creator.bat
@@ -0,0 +1,15 @@
+@echo off
+
+set /p arg="Enviame esa lista hermano: "
+set count=0
+
+:: itero en los argumentos
+for %%a in (%arg%) do (
+    IF NOT EXIST %%a (
+        echo. > %%a
+    ) ELSE (
+        echo %%a:  ya existo brother
+    )
+)
+
+pause
